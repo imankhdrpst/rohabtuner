45ba361879f6de8f391f9055fd37d60e060662c9
Iman Khodaparast <i.khodaparast@inpin.co>
2017-10-18 13:40:23 +0330

    new appId

---
+++ app/build/generated/source/buildConfig/persiandemoplay/debug/com/soutazin/turkishtuner/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.soutazin.turkishtuner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.soutazin.persiantunerdemonew";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "persiandemoplay";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.2";
+  // Fields from product flavor: persiandemoplay
+  public static final String NATIONALITY = "Persian";
+  public static final String PAID_VERSION = "com.soutazin.persiantuner";
+  public static final String PLATFORM = "play";
+  public static final String TYPE = "PERSIAN_DEMO";
+}
